6023cdf7cfdd718bbe438f5e345e4104f7aeda8a
n-takaku <n.takaku@bee-partners.co.jp>
2017-06-17 16:20:55 +0900

    Project's Initial Commit

---
+++ www/js/page/camera.js
@@ -0,0 +1,15 @@
+// This is a JavaScript file
+
+// スワイプを検知したあとの処理
+function swipeEvent(direction) {
+    if (direction == 'right'){
+      alert('右');
+    } else if (direction == 'left'){
+       alert('左');
+      location.href = "../index.html";
+    } else if (direction == 'up'){
+      alert('上');
+    } else if (direction == 'down'){
+       alert('下');
+    }
+}
---
+++ www/js/page/picList.js
@@ -0,0 +1,16 @@
+// This is a JavaScript file
+
+// スワイプを検知したあとの処理
+function swipeEvent(direction) {
+    if (direction == 'right'){
+      alert('右');
+      location.href = "page/camera.html";
+    } else if (direction == 'left'){
+       alert('左');
+      location.href = "page/picSelect.html";
+    } else if (direction == 'up'){
+      alert('上');
+    } else if (direction == 'down'){
+       alert('下');
+    }
+}
